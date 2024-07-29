6bb740af7da93e1e51e5e52d5d72dfee26754516
Alireza <ar.mahdavi21@gmail.com>
2024-07-28 22:16:54 -0400

    Add files via upload

---
+++ indoor_pm_data_read_in.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Program to read in PM data recorded by DustTrak DRX and merge with outdoor PM2.5 data
+
+@author: alima
+"""
+
+import pandas as pd
+exec(open('C:/Life/5- Career & Business Development/Learning/Python Practice/Generic Codes/notion_corrections.py').read())
+
+### Reading DustTrak PM2.5 data and refining
+df = pd.read_excel(backslash_correct(r'C:\Life\5- Career & Business Development\Learning\Python Practice\Stata_Python_Booster\PhD - QFF\Processed\dt_drx.xlsx'))
+df = df[df['visit'] != 5]
+df.rename(columns = {'Date': 'Time'}, inplace = True)
+df.drop('visit', axis = 1, inplace = True)
+df.reset_index(drop = True, inplace = True)
+
+
+### Reading previously saved Outdoor PM2.5 data
+odpm = pd.read_excel(backslash_correct(r'C:\Life\5- Career & Business Development\Learning\Python Practice\Stata_Python_Booster\PhD - QFF\Processed\outdoor_pm25.xlsx'))
+df = pd.merge(df, odpm, on = 'Time', how = 'inner')
+df.rename(columns = {'PM2.5_x': 'PM2.5', 'PM2.5_y': 'PM2.5_OD'}, inplace = True)
+
+### Re-arranging and saving
+df = df[['Time', 'PM1', 'PM10', 'TSP', 'PM2.5', 'PM2.5_OD']]
+df.to_excel(backslash_correct(r'C:\Life\5- Career & Business Development\Learning\Python Practice\Stata_Python_Booster\PhD - QFF\Processed\all_pm.xlsx'), index = False)
